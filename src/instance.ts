6998c497690499747b7f2a0615703c8109f68cf0
clevok <1029794653@qq.com>
2020-06-11 09:48:38 +0800

    [update]

--- src/instance.ts
+++ src/instance.ts
@@ -2,47 +2,40 @@ export type IBindings = Record<string, any> | void
 
 export type IContext = any
 
-export type ISetup<Props extends Record<string, any>> = (
-    this: void,
-    props: Props
-) => IBindings
+export type ISetup<Props extends Record<string, any>> = (this: void, props: Props) => IBindings
 
-export function getContext() {
-    
-}
+export function getContext (){}
 
 /** 执行期间的页面 */
-let currentModule: CurrentModuleInstance | null = null
+let currentModule: ICurrentModuleInstance | null = null
 
-export type CurrentModuleInstance =
-    | (WechatMiniprogram.Component.InstanceProperties &
-          WechatMiniprogram.Component.InstanceMethods<
-              Record<string, unknown>
-          > & {
-              [key: string]: any
-          })
-    | (WechatMiniprogram.Page.InstanceProperties &
-          WechatMiniprogram.Page.InstanceMethods<Record<string, unknown>> & {
-              [key: string]: any
-          })
+export type ICurrentModuleInstance =
+	| (WechatMiniprogram.Component.InstanceProperties &
+			WechatMiniprogram.Component.InstanceMethods<Record<string, unknown>> & {
+				[key: string]: any
+			})
+	| (WechatMiniprogram.Page.InstanceProperties &
+			WechatMiniprogram.Page.InstanceMethods<Record<string, unknown>> & {
+				[key: string]: any
+			})
 
-export function getCurrentInstance(): CurrentModuleInstance | null {
-    return currentModule
+export function getCurrentInstance (): ICurrentModuleInstance | null{
+	return currentModule
 }
 
 /**
  * 接受第一个参数是 current对象
  * @param callback
  */
-export function overCurrentModule<T extends Function>(callback: T): T {
-    // @ts-ignore
-    return function (target: CurrentModuleInstance, ...arg: any[]) {
-        currentModule = target
+export function overCurrentModule<T extends Function> (callback: T): T{
+	// @ts-ignore
+	return function (target: ICurrentModuleInstance, ...arg: any[]){
+		currentModule = target
 
-        const reuslt = callback.call(target, target, ...arg)
+		const reuslt = callback.call(target, target, ...arg)
 
-        currentModule = null
+		currentModule = null
 
-        return reuslt
-    }
+		return reuslt
+	}
 }
--- src/shared.ts
+++ src/shared.ts
@@ -3,65 +3,65 @@ import { useEffect, isObserve } from './watch'
 import { isPlainObject, isArray, isFunction } from './utils'
 import { diff } from './diff'
 import { overCloneDeep } from './over'
-import { overCurrentModule, CurrentModuleInstance } from './instance'
+import { overCurrentModule, ICurrentModuleInstance } from './instance'
 
-export const deepToRaw = overCloneDeep(function (x: unknown) {
-    if (isRef(x)) {
-        return x.value
-    }
-    if (isArray(x)) {
-        return x.map((item) => deepToRaw(item))
-    }
-    if (isPlainObject(x)) {
-        const obj: { [key: string]: unknown } = {}
-        Object.keys(x).forEach((key) => {
-            obj[key] = deepToRaw(x[key])
-        })
-        return obj
-    }
+export const deepToRaw = overCloneDeep(function (x: unknown){
+	if (isRef(x)) {
+		return x.value
+	}
+	if (isArray(x)) {
+		return x.map((item) => deepToRaw(item))
+	}
+	if (isPlainObject(x)) {
+		const obj: { [key: string]: unknown } = {}
+		Object.keys(x).forEach((key) => {
+			obj[key] = deepToRaw(x[key])
+		})
+		return obj
+	}
 
-    return x
+	return x
 })
 
 /**
  * Page/Component 与 watch 中转
  * @return {function} 抛弃监听
  */
-export function deepWatch(target: any, key: string, value: any) {
-    const deepEffects: IRef[] = []
-    ;(function observerEffects(x: any) {
-        /**
+export function deepWatch (target: any, key: string, value: any){
+	const deepEffects: IRef[] = []
+	;(function observerEffects (x: any){
+		/**
          * isObserve必须是在最前面,因为会被isPlainObject解析
          */
-        if (isObserve(x)) {
-            return void deepEffects.push(x)
-        }
-        if (isArray(x)) {
-            return void x.map((item) => observerEffects(item))
-        }
-        if (isPlainObject(x)) {
-            return void Object.keys(x).forEach((key) => {
-                observerEffects(x[key])
-            })
-        }
-    })(value)
+		if (isObserve(x)) {
+			return void deepEffects.push(x)
+		}
+		if (isArray(x)) {
+			return void x.map((item) => observerEffects(item))
+		}
+		if (isPlainObject(x)) {
+			return void Object.keys(x).forEach((key) => {
+				observerEffects(x[key])
+			})
+		}
+	})(value)
 
-    if (!deepEffects.length) {
-        return
-    }
+	if (!deepEffects.length) {
+		return
+	}
 
-    return useEffect(() => {
-        target.setData(
-            diff(
-                {
-                    [key]: deepToRaw(value),
-                },
-                {
-                    [key]: target.data[key],
-                }
-            )
-        )
-    }, deepEffects)
+	return useEffect(() => {
+		target.setData(
+			diff(
+				{
+					[key]: deepToRaw(value)
+				},
+				{
+					[key]: target.data[key]
+				}
+			)
+		)
+	}, deepEffects)
 }
 
 /**
@@ -73,29 +73,29 @@ export function deepWatch(target: any, key: string, value: any) {
  * @return {function} - 停止内部所有依赖的监听
  */
 export const setup = overCurrentModule(function (
-    target: CurrentModuleInstance,
-    callback: Function,
-    props: unknown = {}
-) {
-    const binding = callback.call(target, props)
-    const stopHandels = Object.keys(binding).map((key) => {
-        const value = binding[key]
+	target: ICurrentModuleInstance,
+	callback: Function,
+	props: unknown = {}
+){
+	const binding = callback.call(target, props)
+	const stopHandels = Object.keys(binding).map((key) => {
+		const value = binding[key]
 
-        if (isFunction(value)) {
-            target[key] = value
-            return
-        }
+		if (isFunction(value)) {
+			target[key] = value
+			return
+		}
 
-        target.setData({
-            [key]: deepToRaw(value),
-        })
+		target.setData({
+			[key]: deepToRaw(value)
+		})
 
-        return deepWatch(target, key, value)
-    })
+		return deepWatch(target, key, value)
+	})
 
-    return () => {
-        stopHandels.forEach((stopHandle) => {
-            stopHandle && stopHandle()
-        })
-    }
+	return () => {
+		stopHandels.forEach((stopHandle) => {
+			stopHandle && stopHandle()
+		})
+	}
 })
