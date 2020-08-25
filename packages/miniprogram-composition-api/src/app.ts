8008435fdde51f8108d6ef0362ecd51fd296f795
clevok <1029794653@qq.com>
2020-08-25 18:12:25 +0800

    [add]

--- packages/miniprogram-composition-api/src/app.ts
+++ packages/miniprogram-composition-api/src/app.ts
@@ -1,20 +1,36 @@
 defineApp({
-    // 路由定义规则, 遍历数组,找到对应别名alias,跳转到该page,如果没有找到,则尝试直接跳转页面
-    router: [
-        {
-            // 路径
-            page: '/page/index/index',
-            // 别名
-            alias: 'index'
-        },
-        {
-            page: '/page/message/message',
-            // 重定向 {query, params}, query是路由跳转载荷, params是路径上的参数对象
-            // to,要前往的页面, from来自哪个页面
-            // next传入页面对象
-            beforeEnter: (to, from, next) => {
-                
+    // 跳转规则, 找到对应别名alias/page, 匹配到别名采用别名,没有则匹配page, 最后 则尝试直接跳转页面
+    router: {
+        tabbar: [
+            {
+                // 路径
+                page: '/page/index/index',
+                // 别名
+                alias: 'index'
             }
-        }
-    ]
+        ],
+        pages: [
+            {
+                // 路径
+                page: '/page/index/index',
+                // 别名
+                alias: 'index'
+            },
+            {
+                page: '/page/message/message',
+                // 重定向 {query, params}, query 是路由跳转载荷, params 是路径上的参数对象
+                // to,要前往的页面, from来自哪个页面
+                // next传入页面对象
+                beforeEnter: (to, from, next) => {
+                    
+                    // 则开始匹配这个规则
+                    next({
+                        path: '',
+                        params: {},
+                        query: {}
+                    })
+                }
+            }
+        ]
+    }
 })
